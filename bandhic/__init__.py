edd4b4acd85167f8a397e29ac12e8730018b77c4
wwb <wangweibing@xidian.edu.cn>
2025-08-07 20:35:22 +0800

    Bump version: 0.1.6 → 0.1.7

--- bandhic/__init__.py
+++ bandhic/__init__.py
@@ -7,4 +7,4 @@ from ._test_utils import *
 from .utils.HiCKRy import *
 from .utils.topdom import *
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
 
 setup(
     name="bandhic",
-    version="0.1.6",
+    version="0.1.7",
     author="Weibing Wang",
     description="BandHiC: a memory-efficient Python package for managing and analyzing Hi-C data down to sub-kilobase resolution",
     long_description=long_description,
